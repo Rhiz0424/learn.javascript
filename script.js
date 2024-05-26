4fe4418590bbe952063ab1ab9157d3cfca0fbba9
Rhizzz <168879185+Rhiz0424@users.noreply.github.com>
2024-05-26 18:46:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,7 @@ function identitas() {
   let nama = prompt('Masukan nama anda :')
   let tanggalLahir = prompt('Masukanasukan tanggal lahir anda :')
   let email = prompt('masukan alamat email anda :')
-  alert(`Nama anda adalah : ${nama}, tanggal lahir anda : ${tanggalLahir}, alamat email anda : ${email}`)
+  alert(`Nama anda adalah : ${nama}
+Tanggal lahir anda : ${tanggalLahir}
+Alamat email anda : ${email}`)
 }

4cb754b977d8156e446fd1ffc1543f99a3ed1fce
Rhizzz <168879185+Rhiz0424@users.noreply.github.com>
2024-05-26 09:01:30 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function identitas() {
+  let nama = prompt('Masukan nama anda :')
+  let tanggalLahir = prompt('Masukanasukan tanggal lahir anda :')
+  let email = prompt('masukan alamat email anda :')
+  alert(`Nama anda adalah : ${nama}, tanggal lahir anda : ${tanggalLahir}, alamat email anda : ${email}`)
+}
